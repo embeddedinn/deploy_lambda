154765eab07980dd981ac77869022383d72939c3
C16658 <vysakh.pillai@microchip.com>
2022-07-04 17:43:39 -0700

    deploy_lambda action

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,4 @@
 #!/bin/sh -l
 
 AWS_ACCESS_KEY_ID=$1 AWS_SECRET_ACCESS_KEY=$2 AWS_DEFAULT_REGION=$3 \
-aws lambda update-function-code --function-name $4  --zip-file $5
+aws lambda update-function-code --function-name $4  --zip-file fileb://$5
